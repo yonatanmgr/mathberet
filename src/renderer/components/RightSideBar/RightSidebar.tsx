ad6369ab381ef81d5bb486337fb0b1f003400c33
Erez Birenholz <biran330@gmail.com>
2023-03-03 02:35:32 +0200

    Remove redundant parentheses

--- src/renderer/components/RightSideBar/RightSidebar.tsx
+++ src/renderer/components/RightSideBar/RightSidebar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './RightSidebar.scss';
 import SidebarButton from './SidebarButton';
 import FileSystem from './FileSystem';
-import { useKBar } from 'kbar';
 
 const RightSidebar = () => {
   const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(false);
@@ -10,7 +9,6 @@ const RightSidebar = () => {
   const handleOnClickFiles = () => {
     setIsRightSidebarOpen((isRightSidebarOpen) => !isRightSidebarOpen);
   };
-
 
   const handleOnClickMathPanel = () => {
     //todo: should be refactored to React way
@@ -21,38 +19,29 @@ const RightSidebar = () => {
     <div className={'right-sidebar' + (isRightSidebarOpen ? ' open' : '')}>
       <div className='basic'>
         <section id='top'>
-
           <SidebarButton
             title='המחברות שלי'
             buttonType='files'
-            icon={'notebook'}
+            icon='notebook'
             onClick={handleOnClickFiles}
-          />  
-          <SidebarButton
-            title='חיפוש'
-            buttonType='search'
-            icon={'terminal'}
           />
+          <SidebarButton title='חיפוש' buttonType='search' icon='terminal' />
         </section>
         <section id='bottom'>
-
           <SidebarButton
             title='זיכרון מתמטי'
             buttonType='mathPanel'
-            icon={'calculator'}
+            icon='calculator'
             onClick={handleOnClickMathPanel}
           />
-          <SidebarButton
-            title='הארכיון'
-            buttonType='archive'
-            icon={'archive'}
-          />
-
+          <SidebarButton title='הארכיון' buttonType='archive' icon='archive' />
         </section>
       </div>
-      {isRightSidebarOpen && <section className='extension'>
-      <FileSystem/>
-      </section>}
+      {isRightSidebarOpen && (
+        <section className='extension'>
+          <FileSystem />
+        </section>
+      )}
     </div>
   );
 };
