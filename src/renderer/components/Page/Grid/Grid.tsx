56456d4c06e857ed5d4df9534be9639ece7a242e
Erez Birenholz <biran330@gmail.com>
2023-03-05 14:13:28 +0200

    convert to function comp + add from toolbar

--- src/renderer/components/Page/Grid/Grid.tsx
+++ src/renderer/components/Page/Grid/Grid.tsx
@@ -1,142 +1,67 @@
 import '../Page.scss';
 import React, { useEffect, useState } from 'react';
-import { GridStack, GridStackWidget } from 'gridstack';
 import 'gridstack/dist/gridstack.css';
-import GridBlock from './Block';
 // import { renderToString } from 'react-dom/server';
 import { useGeneralContext } from '@components/GeneralContext';
 import { newWidgetRequest, WidgetType } from '@renderer/common/types';
 import Geogebra from 'react-geogebra';
 import TextBlockContent from './TextBlock';
-import _ from "lodash";
+import _ from 'lodash';
 
-import GridLayout, { Responsive, WidthProvider } from "react-grid-layout";
+import { Responsive, WidthProvider } from 'react-grid-layout';
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
-function blockData(
-  html: JSX.Element,
-  id: number,
-  type: string,
-  h: number,
-  blockContent = {},
-  x = 12,
-  y = 1000,
-  w = 6,
-  minW = 2,
-  minH = 2,
-  maxH = 1000,
-  maxW = 12,
-) {
-  return {
-    id: id,
-    content: html,
-    x: x,
-    y: y,
-    h: h,
-    w: w,
-    minW: minW,
-    minH: minH,
-    type: type,
-    blockContent: blockContent,
-    maxH: maxH,
-    maxW: maxW,
-  } as GridStackWidget;
-}
+const PageGrid = (props) => {
+  const {
+    className = 'layout',
+    cols = { lg: 8, md: 6, sm: 4, xs: 2, xxs: 1 },
+    rowHeight = 50,
+    isBounded = true,
+    resizeHandles = ['sw'],
+    containerPadding = [0, 0],
+    breakpoints = { lg: 800, md: 600, sm: 400, xs: 200, xxs: 100 },
+  } = props;
 
+  const [state, setState] = useState({
+    items: [],
+    newCounter: 0,
+  });
 
-const PageGrid = () => {
-  class Grid extends React.PureComponent {
-    static defaultProps = {
-      className:"layout",
-      cols:{ lg: 8, md: 6, sm: 4, xs: 2, xxs: 1 },
-      rowHeight:50,
-      isBounded:true,
-      resizeHandles:['sw'],
-      draggableHandle: '.block-handle',
-      containerPadding:[0,0],
-      breakpoints:{ lg: 800, md: 600, sm: 400, xs: 200, xxs: 100 },
-      onLayoutChange: function () {}
-    };
-  
-    constructor(props: object) {
-      super(props);
-  
-      this.state = {
-        items: [],
-        newCounter: 0
-      };
-  
-      this.onAddItem = this.onAddItem.bind(this);
-      this.onBreakpointChange = this.onBreakpointChange.bind(this);
-    }
-  
-    createElement(el) {
-      const i = el.add ? "+" : el.i;
-      return (
-        <div className='block' data-grid={el} key={i}>
-          <div className='block-handle'>
-            <i className='fi fi-rr-menu-dots-vertical' />
-          </div>
-          <div className='block-content'>
-            <span className="block-remove-button" onClick={this.onRemoveItem.bind(this, i)}>
-              <i className='fi fi-rr-x'></i>
-            </span>
-            <TextBlockContent></TextBlockContent>
-          </div>
+  const createElement = (el) => {
+    const i = el.add ? '+' : el.i;
+    return (
+      <div className='block' data-grid={el} key={i}>
+        <div className='block-handle'>
+          <i className='fi fi-rr-menu-dots-vertical' />
         </div>
-      );
-    }
-  
-    onAddItem() {
-      /*eslint no-console: 0*/
-      console.log("adding", "n" + this.state.newCounter);
-      this.setState({
-        // Add a new item. It must have a unique key!
-        items: this.state.items.concat({
-          i: "n" + this.state.newCounter,
-          x: Infinity,
-          y: Infinity, // puts it at the bottom
-          w: 8,
-          h: 2
-        }),
-        // Increment the counter to ensure key is always unique.
-        newCounter: this.state.newCounter + 1
-      });
-    }
-  
-    // We're using the cols coming back from this to calculate where to add new items.
-    onBreakpointChange(breakpoint, cols) {
-      this.setState({
-        breakpoint: breakpoint,
-        cols: cols
-      });
-    }
-  
-    onLayoutChange(layout) {
-      this.props.onLayoutChange(layout);
-      this.setState({ layout: layout });
-    }
-  
-    onRemoveItem(i) {
-      console.log("removing", i);
-      this.setState({ items: _.reject(this.state.items, { i: i }) });
-    }
-  
-    render() {
-      return (
-        <div>
-          <button onClick={this.onAddItem}>Add Item</button>
-          <ResponsiveGridLayout
-            onLayoutChange={this.onLayoutChange}
-            onBreakpointChange={this.onBreakpointChange}
-            {...this.props}
+        <div className='block-content'>
+          <span
+            className='block-remove-button'
+            onClick={onRemoveItem.bind(this, i)}
           >
-            {_.map(this.state.items, el => this.createElement(el))}
-          </ResponsiveGridLayout>
+            <i className='fi fi-rr-x'></i>
+          </span>
+          <TextBlockContent></TextBlockContent>
         </div>
-      );
-    }
-  }
+      </div>
+    );
+  };
+
+  // We're using the cols coming back from this to calculate where to add new items.
+  const onBreakpointChange = (breakpoint, cols) => {
+    setState((prev) => ({ ...prev, breakpoint, cols }));
+  };
+
+  const onLayoutChange = (layout) => {
+    setState((prev) => ({ ...prev, layout }));
+  };
+
+  const onRemoveItem = (i) => {
+    console.log('removing', i);
+    // setItems({ items: _.reject(state.items, { i: i }) });
+    //Todo: not implemented
+    setState((prev) => ({ ...prev, items: [] }));
+  };
 
   const { newWidgetRequest, clearPageRequest } = useGeneralContext();
 
@@ -158,7 +83,6 @@ const PageGrid = () => {
     console.log('page should be cleared');
   }, [clearPageRequest]);
 
-
   const AddWidget = (newWidgetRequest: newWidgetRequest) => {
     console.log(newWidgetRequest);
     const handler = addWidgetHandlersMap.get(newWidgetRequest.widgetType);
@@ -166,7 +90,20 @@ const PageGrid = () => {
   };
 
   function addText() {
-    console.error('not implemented');
+    /*eslint no-console: 0*/
+    console.log('adding', 'n' + state.newCounter);
+    setState({
+      // Add a new item. It must have a unique key!
+      items: state.items.concat({
+        i: 'n' + state.newCounter,
+        x: Infinity,
+        y: Infinity, // puts it at the bottom
+        w: 8,
+        h: 2,
+      }),
+      // Increment the counter to ensure key is always unique.
+      newCounter: state.newCounter + 1,
+    });
   }
 
   function addPicture() {
@@ -175,17 +112,6 @@ const PageGrid = () => {
 
   function addGgb() {
     console.error('not implemented');
-    // const id = Date.now();
-    // const html = 
-    //   <Geogebra
-    //     id={id.toString()}
-    //     width={800}
-    //     height={600}
-    //     showMenuBar
-    //     showToolBar
-    //     showAlgebraInput
-    //     appletOnLoad={() => console.log('appletOnLoad')}
-    //   />
   }
 
   function addMath() {
@@ -197,25 +123,20 @@ const PageGrid = () => {
 
   function addDivider() {
     console.error('not implemented');
-    // const id = Date.now();
-    // const html = <hr className='pageDivider' />;
-    // const block = blockData(
-    //   html,
-    //   id,
-    //   'Divider',
-    //   1,
-    //   {},
-    //   12,
-    //   1000,
-    //   12,
-    //   1,
-    //   1,
-    //   1,
-    //   12,
-    // );
   }
 
-  return <div className='grid-container'><Grid /></div>;
+  return (
+    <div>
+      <button onClick={addText}>Add Item</button>
+      <ResponsiveGridLayout
+        onLayoutChange={onLayoutChange}
+        onBreakpointChange={onBreakpointChange}
+        {...props}
+      >
+        {_.map(state.items, (el) => createElement(el))}
+      </ResponsiveGridLayout>
+    </div>
+  );
 };
 
 export default PageGrid;
