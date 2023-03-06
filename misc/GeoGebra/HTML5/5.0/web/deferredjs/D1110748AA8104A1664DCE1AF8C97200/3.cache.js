beef7937e26ee4c1fef3db3a10a8aa09bba4cd53
yonatanmgr <yonatanmgr@gmail.com>
2023-03-06 19:47:56 +0200

    removing geogebra for now

--- misc/GeoGebra/HTML5/5.0/web/deferredjs/D1110748AA8104A1664DCE1AF8C97200/3.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.web.runAsyncCallback3('GAc(3503,1,mCm);_.te=function(){};_Nl(ki)(3);\n//# sourceURL=web-3.js\n')
--- misc/GeoGebra/HTML5/5.0/webSimple/deferredjs/FD44D89B7EB5EC526B54B5CA23995346/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.webSimple.runAsyncCallback2('zAb(1964,1,XFh);_.zd=function(){};l4g(Ng)(2);\n//# sourceURL=webSimple-2.js\n')
