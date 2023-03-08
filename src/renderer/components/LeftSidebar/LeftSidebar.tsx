806003efd186aa02df21a9f526866029a7d058c3
yonatanmgr <yonatanmgr@gmail.com>
2023-03-08 21:04:48 +0200

    rename title in left sidebar

--- src/renderer/components/LeftSidebar/LeftSidebar.tsx
+++ src/renderer/components/LeftSidebar/LeftSidebar.tsx
@@ -3,7 +3,7 @@ import ActionsGroup from './ActionsGroup';
 
 const LeftSidebar = () => {
   return <div className='left-sidebar'>
-    <ActionsGroup title='הצבות' groupType='definitions'/>
+    <ActionsGroup title='משתנים' groupType='variables'/>
     <ActionsGroup title='פונקציות' groupType='functions'/>
   </div>;
 };
