4f332e12f53dde4053016aafea9446c547a9834f
yonatanmgr <yonatanmgr@gmail.com>
2023-03-09 15:38:55 +0200

    started with tags

--- src/renderer/components/Header/AddTag.tsx
+++ src/renderer/components/Header/AddTag.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AddTag = () => {
-  return (
-    <div className='tag-pill new'>
+  const [clickState, setClickState] = useState(false);
+  const [currentValue, setCurrentValue] = useState('');
+  const [savedValue, setSavedValue] = useState('');
+
+  const addTagStyle = {
+    backgroundColor: 'unset',
+    height: '13px',
+    border: 'none',
+    color: 'unset',
+    fontFamily: 'unset',
+    fontSize: 'unset',
+    minWidth: `92px`,
+    width: `${currentValue.length * 9 + 10}px`,
+    outline: 'none',
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setSavedValue(currentValue);
+      setClickState(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log(`Add tag with vaule ${savedValue}`);
+  }, [savedValue]);
+
+  useEffect(() => {
+    setCurrentValue('');
+  }, [clickState]);
+
+  const content = clickState ? (
+    <input
+      autoFocus={true}
+      type='text'
+      title=''
+      placeholder='+ הוספת תגית'
+      className='tag-text-adding'
+      onChange={(e) => setCurrentValue(e.target.value)}
+      onKeyDown={handleKeyDown}
+      style={addTagStyle}
+    />
+  ) : (
+    <>
       <span className='add'>+ </span>
       <span className='tag-text'>הוספת תגית</span>
+    </>
+  );
+
+  return (
+    <div className='tag-pill new' onClick={() => setClickState(true)}>
+      {content}
     </div>
   );
 };
