573a2fd61659ef0927ce0a10fb77450a0dde19bb
yonatanmgr <yonatanmgr@gmail.com>
2023-03-12 00:16:22 +0200

    only render grid when opening file

--- src/renderer/components/Page/Page.tsx
+++ src/renderer/components/Page/Page.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Application.scss';
-
 
 import PageGrid from './Grid/Grid';
 import './Page.scss';
 
-
-
 import ToolsPanel from './ToolsPanel/ToolsPanel';
-import './ToolsPanel/ToolsPanel.scss'
+import './ToolsPanel/ToolsPanel.scss';
 
+import { useGeneralContext } from '@components/GeneralContext';
 
 
 const Page = () => {
+  const { selectedFile } = useGeneralContext();
+  const [currentFilePath, setCurrentFilePath] = useState("")
+
+  useEffect(() => {
+    setCurrentFilePath(selectedFile)
+  }, [selectedFile])
+
   return (
     <div className='page'>
-      <div className='page-grid'>
-        <PageGrid />
-        <ToolsPanel />
-      </div>
+      { currentFilePath ?
+        <div className='page-grid'>
+          <PageGrid />
+          <ToolsPanel />
+        </div> : <></>
+      }
     </div>
   );
 };
